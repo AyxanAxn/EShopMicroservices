37e7caba19d09d771ee8d6fb8b36908994fc2409
Aykhan <axundovayxan9@gmail.com>
2024-05-18 17:37:28 +0400

    Added other order solutions

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,5 +1,17 @@
+using Ordering.Application;
+using Ordering.Infrastructure;
+using Ordering.API;
+
 var builder = WebApplication.CreateBuilder(args);
 
+#region Added services
+builder.Services.AddApiServices()
+                .AddApplicationServices()
+                .AddInfrastructureServices(builder.Configuration);
+#endregion
+
 var app = builder.Build();
+
+app.UseApiServices();
 
 app.Run();
