d90bc436dac26d8e97273449a7fe76117fe6799b
Aykhan <axundovayxan9@gmail.com>
2024-05-18 16:21:44 +0400

    Added Ordering.API empty project

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,6 @@
-
 var builder = WebApplication.CreateBuilder(args);
+
+#region Application Services
 var dbConnection = builder.Configuration.GetConnectionString("Database")!;
 var redisConnection = builder.Configuration.GetConnectionString("Redis")!;
 var assembly = typeof(Program).Assembly;
@@ -13,13 +14,16 @@ builder
         config.RegisterServicesFromAssemblies(assembly);
     });
 
+builder.Services.AddCarter();
+#endregion
+
+#region Data Services
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(dbConnection);
     opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
 
-builder.Services.AddCarter();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
@@ -29,20 +33,29 @@ builder.Services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = redisConnection;
 });
+#endregion
 
-builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
-    (options =>
-    {
-        options.Address = new Uri(builder
-            .Configuration["GrpcSettings:DiscountUrl"]!);
-    });
+#region Grpc Services
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+{
+    ServerCertificateCustomValidationCallback =
+    HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+});
+#endregion
 
+#region Cross - Cutting Services
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 builder.Services.AddHealthChecks()
     .AddNpgSql(dbConnection)
     .AddRedis(redisConnection);
+#endregion
 
+#region Configure the HTTP request pipeline.
 var app = builder.Build();
 
 app.UseExceptionHandler(options => { });
@@ -56,3 +69,4 @@ app.UseHealthChecks("/health",
 app.MapCarter();
 
 app.Run();
+#endregion
